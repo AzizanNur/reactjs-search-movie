79051c1386921b1a6a2d402652acbee4d0eba1bf
Azizan Nur Rohman <azizan@rschooltoday.com>
2023-02-12 22:14:24 +0700

    search movie

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {getMovieList, searchMovie} from './api';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () =>  {
+
+  const [popularsMovies, setPopularMovies] = useState([]);
+
+  useEffect(()=>{
+    getMovieList().then((result) => {
+      setPopularMovies(result) 
+    })
+  }, [])
+
+  const PopularMoviesList = () => {
+    return popularsMovies.map((movie, i) => {
+      return (
+        <div className="Movie-wrapper" key={i}>
+            <div className="Movie-title">{movie.title}</div>
+            <img 
+              className="Movie-img" 
+              alt=""
+              src={`${process.env.REACT_APP_BASEIMGURL}${movie.poster_path}`}
+            />
+            <div className="Movie-date">{movie.release_date}</div>
+            <div className="Movie-rate">{movie.vote_average}</div>
+        </div>
+      )
+    })
+  }
+
+  const search = async (q) => {
+   if(q.length >= 3){
+    const query = await searchMovie(q);
+    setPopularMovies(query)
+   }
+  }
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Azizan Search Movie</h1>
+        <input 
+          placeholder='Seach Movie....' 
+          class="Movie-search"
+          onChange={({target}) => search(target.value)}
+        />
+        <div className="Movie-container">
+          <PopularMoviesList />      
+        </div>
       </header>
     </div>
   );
